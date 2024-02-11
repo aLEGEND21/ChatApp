5ae92bfa8142b53d28da214a636d90cc77bb4fec
aLEGEND21 <arnavmurthi@gmail.com>
2024-02-11 13:07:50 -0500

    chore: Enable logging in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -24,7 +24,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 # Configure the socket interface
-socketio = SocketIO(app)
+socketio = SocketIO(app, logger=True, engineio_logger=True, cors_allowed_origins="*")
 
 
 # Socket events
