c36071d26a53c9b06307391fcfd656416adcd6d0
aLEGEND21 <arnavmurthi@gmail.com>
2024-02-11 13:10:28 -0500

    ops: Preserve .env file in update script

--- chatapp.update.sh
+++ chatapp.update.sh
@@ -1,6 +1,8 @@
 # This should only be run directly and located one directory above the project
 echo y | docker image prune
-rm -rf chatapp
+mv ChatApp/.env .env
+rm -rf ChatApp
 git clone https://github.com/aLEGEND21/ChatApp.git
+mv .env ChatApp/.env
 cd ChatApp
 bash run.sh
